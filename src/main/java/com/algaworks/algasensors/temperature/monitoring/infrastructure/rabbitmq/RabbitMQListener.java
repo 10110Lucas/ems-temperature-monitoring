c0685c4eec2cf5b8efbcb519f985852297c0836f
10110Lucas <lucas_900azul@hotmail.com>
2025-08-22 05:01:33 -0300

    feat: Persistindo dados de temperatura da fila do RabbitMQ (aula 12.15)

--- src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
@@ -1,17 +1,15 @@
 package com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq;
 
 import com.algaworks.algasensors.temperature.monitoring.api.model.TemperatureLogData;
-import io.hypersistence.tsid.TSID;
+import com.algaworks.algasensors.temperature.monitoring.domain.service.TemperatureMonitoringService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
-import java.util.Map;
 
 import static com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq.RabbitMQConfig.QUEUE;
 
@@ -20,15 +18,12 @@ import static com.algaworks.algasensors.temperature.monitoring.infrastructure.ra
 @RequiredArgsConstructor
 public class RabbitMQListener {
 
+    private final TemperatureMonitoringService service;
+
     @SneakyThrows
     @RabbitListener(queues = QUEUE)
-    public void handle(@Payload TemperatureLogData data, @Headers Map<String, Object> headers) {
-        TSID sensorId = data.getSensorId();
-        Double value = data.getValue();
-
-        log.info("Headers: {}", headers);
-        log.info("Temperature updated: SensorId {} Temp {}", sensorId, value);
-
+    public void handle(@Payload TemperatureLogData data) {
+        service.processTemperatureReading(data);
         Thread.sleep(Duration.ofSeconds(5));
     }
 }
